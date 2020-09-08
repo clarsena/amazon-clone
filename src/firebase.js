2ea94af865b58e291de90d21f7ae544f3e35c5f4
Christopher Arsenault <christopher.arsenault82@gmail.com>
2020-09-08 15:02:44 -0400

    Initial commit

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+	apiKey: 'AIzaSyB5ntLqIS1ej0a-Q-gU-lk7aBBDFFspY2g',
+	authDomain: 'clone-d23ae.firebaseapp.com',
+	databaseURL: 'https://clone-d23ae.firebaseio.com',
+	projectId: 'clone-d23ae',
+	storageBucket: 'clone-d23ae.appspot.com',
+	messagingSenderId: '869281287682',
+	appId: '1:869281287682:web:def1c5fd85b10b4935611b',
+};
