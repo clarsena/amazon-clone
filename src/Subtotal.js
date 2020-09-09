46c8c01fae09ddb3e78ff2ce47c6e19fe2b9ef19
Christopher Arsenault <christopher.arsenault82@gmail.com>
2020-09-09 15:43:17 -0400

    Added items to checkout basket, started login page

--- src/Subtotal.js
+++ src/Subtotal.js
@@ -2,24 +2,19 @@ import React from 'react';
 import './Subtotal.css';
 import CurrencyFormat from 'react-currency-format';
 import { useStateValue } from './StateProvider';
+import { getBasketTotal } from './reducer';
 
 function Subtotal() {
 	const [{ basket }, dispatch] = useStateValue();
-	const getBasketTotal = () => {
-		let total = 0;
-		basket.map((item) => {
-			total += item.price;
-		});
-		return total;
-	};
+
 	return (
 		<div className='subtotal'>
 			<CurrencyFormat
 				renderText={(value) => (
 					<>
 						<p>
-							Subtotal ({basket.length} items):
-							<strong>{` ${value}`}</strong>
+							Subtotal ({basket.length} items):{' '}
+							<strong>{value}</strong>
 						</p>
 						<small className='subtotal__gift'>
 							<input type='checkbox' /> This order contains a gift
@@ -28,7 +23,7 @@ function Subtotal() {
 				)}
 				decimalScale={2}
 				// value={getBasketTotal(basket)}
-				value={getBasketTotal()}
+				value={getBasketTotal(basket)}
 				displayType={'text'}
 				thousandSeparator={true}
 				prefix={'$'}
